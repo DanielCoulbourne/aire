295bc604eb2edea09fe2439d313a1c6d95fab3f1
Daniel Coulbourne <daniel.coulbourne@gmail.com>
2020-04-21 03:05:00 -0400

    Add a failure if AireSelect isn't installed.

--- src/Elements/Select.php
+++ src/Elements/Select.php
@@ -11,20 +11,29 @@ use Galahad\Aire\Elements\Concerns\MapsValueToJsonValue;
 
 class Select extends \Galahad\Aire\DTD\Select implements HasJsonValue
 {
-	use HasValue, HasOptions, AutoId, MapsValueToJsonValue;
-	
-	public function __construct(Aire $aire, $options, Form $form = null)
-	{
-		parent::__construct($aire, $form);
-		
-		$this->setOptions($options);
-		
-		$this->attributes->registerMutator('name', function($name) {
-			return $this->attributes->get('multiple', false)
-				? preg_replace('/\[\]$/', '', $name).'[]'
-				: $name;
-		});
-		
-		$this->registerAutoId();
-	}
+    use HasValue, HasOptions, AutoId, MapsValueToJsonValue;
+    
+    public function __construct(Aire $aire, $options, Form $form = null)
+    {
+        parent::__construct($aire, $form);
+        
+        $this->setOptions($options);
+        
+        $this->attributes->registerMutator('name', function ($name) {
+            return $this->attributes->get('multiple', false)
+                ? preg_replace('/\[\]$/', '', $name).'[]'
+                : $name;
+        });
+        
+        $this->registerAutoId();
+    }
+
+    public function withJavascript($config = [])
+    {
+        if (! class_exists(InterNACHI\AireSelect::class)) {
+            throw new \Exception("To use the `Select::withJavascript()` method, please run `composer install internachi/aire-select`", 1);
+        }
+        
+        return new InterNACHI\AireSelect($this, $config);
+    }
 }
--- tests/Unit/SelectTest.php
+++ tests/Unit/SelectTest.php
@@ -7,122 +7,131 @@ use Galahad\Aire\Tests\TestCase;
 
 class SelectTest extends TestCase
 {
-	public function test_an_associative_array_maps_to_value_and_label() : void
-	{
-		$options = [
-			'coates' => 'Ta-Nehisi Coates',
-			'roth' => 'Philip Roth',
-			'patchett' => 'Ann Patchett',
-		];
-		
-		$html = $this->aire()->select($options)->render();
-		
-		$this->assertSelectorTextEquals($html, 'option[value="coates"]', 'Ta-Nehisi Coates');
-		$this->assertSelectorTextEquals($html, 'option[value="roth"]', 'Philip Roth');
-		$this->assertSelectorTextEquals($html, 'option[value="patchett"]', 'Ann Patchett');
-	}
-	
-	public function test_a_zero_index_non_associative_array_maps_value_index() : void
-	{
-		$options = [
-			0 => 'Ta-Nehisi Coates',
-			1 => 'Philip Roth',
-			2 => 'Ann Patchett',
-		];
-		
-		$html = $this->aire()->select($options)->render();
-		
-		$this->assertSelectorTextEquals($html, 'option[value="0"]', 'Ta-Nehisi Coates');
-		$this->assertSelectorTextEquals($html, 'option[value="1"]', 'Philip Roth');
-		$this->assertSelectorTextEquals($html, 'option[value="2"]', 'Ann Patchett');
-	}
-	
-	public function test_an_empty_option_can_be_prepended() : void
-	{
-		$options = [
-			'coates' => 'Ta-Nehisi Coates',
-			'roth' => 'Philip Roth',
-			'patchett' => 'Ann Patchett',
-		];
-		
-		$html = $this->aire()->select($options)->prependEmptyOption('None')->render();
-		
-		$this->assertSelectorTextEquals($html, 'option[value=""]', 'None');
-		$this->assertSelectorTextEquals($html, 'option[value="coates"]', 'Ta-Nehisi Coates');
-		$this->assertSelectorTextEquals($html, 'option[value="roth"]', 'Philip Roth');
-		$this->assertSelectorTextEquals($html, 'option[value="patchett"]', 'Ann Patchett');
-	}
-	
-	public function test_a_custom_empty_option_can_be_prepended() : void
-	{
-		$options = [
-			'coates' => 'Ta-Nehisi Coates',
-			'roth' => 'Philip Roth',
-			'patchett' => 'Ann Patchett',
-		];
-		
-		$html = $this->aire()->select($options)->prependEmptyOption('All', 'all')->render();
-		
-		$this->assertSelectorTextEquals($html, 'option[value="all"]', 'All');
-		$this->assertSelectorTextEquals($html, 'option[value="coates"]', 'Ta-Nehisi Coates');
-		$this->assertSelectorTextEquals($html, 'option[value="roth"]', 'Philip Roth');
-		$this->assertSelectorTextEquals($html, 'option[value="patchett"]', 'Ann Patchett');
-	}
-	
-	public function test_an_empty_option_can_be_prepended_to_a_non_associative_array() : void
-	{
-		$options = [
-			0 => 'Ta-Nehisi Coates',
-			1 => 'Philip Roth',
-			2 => 'Ann Patchett',
-		];
-		
-		$html = $this->aire()->select($options)->prependEmptyOption('Empty')->render();
-		
-		$this->assertSelectorTextEquals($html, 'option[value=""]', 'Empty');
-		$this->assertSelectorTextEquals($html, 'option[value="0"]', 'Ta-Nehisi Coates');
-		$this->assertSelectorTextEquals($html, 'option[value="1"]', 'Philip Roth');
-		$this->assertSelectorTextEquals($html, 'option[value="2"]', 'Ann Patchett');
-	}
-	
-	public function test_an_enum_class_name_will_be_converted_to_a_selectable_array() : void
-	{
-		if (!class_exists('BenSampo\Enum\Enum')) {
-			$this->markTestSkipped();
-			return;
-		}
-		
-		$html = $this->aire()
-			->select(SelectTestOptionsEnum::class)
-			->defaultValue(SelectTestOptionsEnum::AnnPatchett)
-			->render();
-		
-		$this->assertSelectorTextEquals($html, 'option[value="0"]', 'Ta-Nehisi Coates');
-		$this->assertSelectorTextEquals($html, 'option[value="1"]', 'Philip Roth');
-		$this->assertSelectorTextEquals($html, 'option[value="2"]', 'Ann Patchett');
-		$this->assertSelectorTextEquals($html, 'option[selected]', 'Ann Patchett');
-	}
+    public function test_an_associative_array_maps_to_value_and_label() : void
+    {
+        $options = [
+            'coates' => 'Ta-Nehisi Coates',
+            'roth' => 'Philip Roth',
+            'patchett' => 'Ann Patchett',
+        ];
+        
+        $html = $this->aire()->select($options)->render();
+        
+        $this->assertSelectorTextEquals($html, 'option[value="coates"]', 'Ta-Nehisi Coates');
+        $this->assertSelectorTextEquals($html, 'option[value="roth"]', 'Philip Roth');
+        $this->assertSelectorTextEquals($html, 'option[value="patchett"]', 'Ann Patchett');
+    }
+    
+    public function test_a_zero_index_non_associative_array_maps_value_index() : void
+    {
+        $options = [
+            0 => 'Ta-Nehisi Coates',
+            1 => 'Philip Roth',
+            2 => 'Ann Patchett',
+        ];
+        
+        $html = $this->aire()->select($options)->render();
+        
+        $this->assertSelectorTextEquals($html, 'option[value="0"]', 'Ta-Nehisi Coates');
+        $this->assertSelectorTextEquals($html, 'option[value="1"]', 'Philip Roth');
+        $this->assertSelectorTextEquals($html, 'option[value="2"]', 'Ann Patchett');
+    }
+    
+    public function test_an_empty_option_can_be_prepended() : void
+    {
+        $options = [
+            'coates' => 'Ta-Nehisi Coates',
+            'roth' => 'Philip Roth',
+            'patchett' => 'Ann Patchett',
+        ];
+        
+        $html = $this->aire()->select($options)->prependEmptyOption('None')->render();
+        
+        $this->assertSelectorTextEquals($html, 'option[value=""]', 'None');
+        $this->assertSelectorTextEquals($html, 'option[value="coates"]', 'Ta-Nehisi Coates');
+        $this->assertSelectorTextEquals($html, 'option[value="roth"]', 'Philip Roth');
+        $this->assertSelectorTextEquals($html, 'option[value="patchett"]', 'Ann Patchett');
+    }
+    
+    public function test_a_custom_empty_option_can_be_prepended() : void
+    {
+        $options = [
+            'coates' => 'Ta-Nehisi Coates',
+            'roth' => 'Philip Roth',
+            'patchett' => 'Ann Patchett',
+        ];
+        
+        $html = $this->aire()->select($options)->prependEmptyOption('All', 'all')->render();
+        
+        $this->assertSelectorTextEquals($html, 'option[value="all"]', 'All');
+        $this->assertSelectorTextEquals($html, 'option[value="coates"]', 'Ta-Nehisi Coates');
+        $this->assertSelectorTextEquals($html, 'option[value="roth"]', 'Philip Roth');
+        $this->assertSelectorTextEquals($html, 'option[value="patchett"]', 'Ann Patchett');
+    }
+    
+    public function test_an_empty_option_can_be_prepended_to_a_non_associative_array() : void
+    {
+        $options = [
+            0 => 'Ta-Nehisi Coates',
+            1 => 'Philip Roth',
+            2 => 'Ann Patchett',
+        ];
+        
+        $html = $this->aire()->select($options)->prependEmptyOption('Empty')->render();
+        
+        $this->assertSelectorTextEquals($html, 'option[value=""]', 'Empty');
+        $this->assertSelectorTextEquals($html, 'option[value="0"]', 'Ta-Nehisi Coates');
+        $this->assertSelectorTextEquals($html, 'option[value="1"]', 'Philip Roth');
+        $this->assertSelectorTextEquals($html, 'option[value="2"]', 'Ann Patchett');
+    }
+    
+    public function test_an_enum_class_name_will_be_converted_to_a_selectable_array() : void
+    {
+        if (!class_exists('BenSampo\Enum\Enum')) {
+            $this->markTestSkipped();
+            return;
+        }
+        
+        $html = $this->aire()
+            ->select(SelectTestOptionsEnum::class)
+            ->defaultValue(SelectTestOptionsEnum::AnnPatchett)
+            ->render();
+        
+        $this->assertSelectorTextEquals($html, 'option[value="0"]', 'Ta-Nehisi Coates');
+        $this->assertSelectorTextEquals($html, 'option[value="1"]', 'Philip Roth');
+        $this->assertSelectorTextEquals($html, 'option[value="2"]', 'Ann Patchett');
+        $this->assertSelectorTextEquals($html, 'option[selected]', 'Ann Patchett');
+    }
+    
+    public function test_with_javascript_throws_exception_if_aire_select_isnt_installed() : void
+    {
+        $this->expectExceptionMessage('To use the `Select::withJavascript()` method, please run `composer install internachi/aire-select`');
+
+        $this->aire()
+            ->select(SelectTestOptionsEnum::class)
+            ->withJavascript();
+    }
 }
 
 if (class_exists('BenSampo\Enum\Enum')) {
-	class SelectTestOptionsEnum extends Enum
-	{
-		public const TaNehisiCoates = 0;
-		
-		public const PhilipRoth = 1;
-		
-		public const AnnPatchett = 2;
-		
-		public static function getDescription($value) : string
-		{
-			switch ($value) {
-				case static::TaNehisiCoates:
-					return 'Ta-Nehisi Coates';
-				case static::PhilipRoth:
-					return 'Philip Roth';
-				case static::AnnPatchett:
-					return 'Ann Patchett';
-			}
-		}
-	}
+    class SelectTestOptionsEnum extends Enum
+    {
+        public const TaNehisiCoates = 0;
+        
+        public const PhilipRoth = 1;
+        
+        public const AnnPatchett = 2;
+        
+        public static function getDescription($value) : string
+        {
+            switch ($value) {
+                case static::TaNehisiCoates:
+                    return 'Ta-Nehisi Coates';
+                case static::PhilipRoth:
+                    return 'Philip Roth';
+                case static::AnnPatchett:
+                    return 'Ann Patchett';
+            }
+        }
+    }
 }
